39ca7e6b4a61f56b4b0f5a871894893f5fedaa4d
Riska Agung <riskaagung11@gmail.com>
2023-06-17 06:06:39 +0700

    Drived/Computed State

--- src/components/Expense/Expenses.jsx
+++ src/components/Expense/Expenses.jsx
@@ -7,6 +7,16 @@ import { useState } from "react";
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
+  let filteredInfoText = "2021, 2022 and 2023";
+
+  if (filteredYear === "2021") {
+    filteredInfoText = "2020, 2022 and 2023";
+  } else if (filteredYear === "2022") {
+    filteredInfoText = "2020, 2021 and 2023";
+  } else if (filteredYear === "2023") {
+    filteredInfoText = "2020, 2021 and 2022";
+  }
+
   const filterChangeHandeler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
@@ -14,6 +24,7 @@ const Expenses = (props) => {
   return (
     <div>
       <Card className="expenses">
+        <p style={{ color: "white" }}>Data for {filteredInfoText} is hidden</p>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandeler} />
         <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
         <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
